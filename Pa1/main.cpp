f46ee4b91c3a439f6f26767b049d436c4b7c003a
Wyatt Burlingame <82528319+Wburling@users.noreply.github.com>
2023-09-05 10:43:12 -0600

    test

--- Pa1/main.cpp
+++ Pa1/main.cpp
@@ -1,11 +1,5 @@
 #include "functions.h"
 
 int main() {
-    char * product[3]={"Milk", "Eggs", "Bread"};
-    double price[3]={2.75, 3.0, 1.69};
-    printf("Product     Price\n");
-    for (int i = 0; i<3; i++){
-        printf("%s   %.2f\n", product[i], price[i]);
-    }
-
+    
 }
