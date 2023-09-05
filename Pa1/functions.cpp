3d9bab0c6d243cfd6c6c3699a8bc69e21c5e2857
Wyatt Burlingame <82528319+Wburling@users.noreply.github.com>
2023-09-05 02:42:01 -0600

    test

---
+++ Pa1/functions.cpp
@@ -0,0 +1,5 @@
+#include "functions.h"
+
+int getTokens(char s[], char **args[]) {
+
+}
--- Pa1/main.cpp
+++ Pa1/main.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
+#include "functions.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    char * product[3]={"Milk", "Eggs", "Bread"};
+    double price[3]={2.75, 3.0, 1.69};
+    printf("Product     Price\n");
+    for (int i = 0; i<3; i++){
+        printf("%s   %.2f\n", product[i], price[i]);
+    }
+
 }
