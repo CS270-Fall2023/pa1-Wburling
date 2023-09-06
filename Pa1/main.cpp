45f57e143c6730d1162ada95a16398278e107b0f
Wyatt Burlingame <82528319+Wburling@users.noreply.github.com>
2023-09-06 01:29:31 -0600

    test

--- Pa1/main.cpp
+++ Pa1/main.cpp
@@ -1,5 +1,8 @@
 #include "functions.h"
-
+#include "stdio.h"
 int main() {
-    
+    char **arrayOfTokens, str[] = "ls -l file";
+    int numberOfToken = getTokens(str, &arrayOfTokens);
+    printf("%s", (*arrayOfTokens)[0]);
+    printf("%d", numberOfToken);
 }
